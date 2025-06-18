e7eaa6e004f903fde45dd84a5b307e8a4511489f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-18 19:52:13 +0000

    camlp5 8.03.05
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/c/camlp5.rb
+++ Formula/c/camlp5.rb
@@ -1,8 +1,8 @@
 class Camlp5 < Formula
   desc "Preprocessor and pretty-printer for OCaml"
   homepage "https://camlp5.github.io/"
-  url "https://github.com/camlp5/camlp5/archive/refs/tags/8.03.01.tar.gz"
-  sha256 "057b8e06590cf29a1bd22b6c83aa5daa816d5cbb2ba2548409d474d7dc10c5b8"
+  url "https://github.com/camlp5/camlp5/archive/refs/tags/8.03.05.tar.gz"
+  sha256 "792fa5df2b04428a53405087d3895d9ab30d20cf9c1509d48186620a7e68a34b"
   license "BSD-3-Clause"
   head "https://github.com/camlp5/camlp5.git", branch: "master"
 
@@ -23,6 +23,7 @@ class Camlp5 < Formula
 
   depends_on "ocaml-findlib" => :build
   depends_on "opam" => :build
+  depends_on "pkgconf" => :build
   depends_on "camlp-streams"
   depends_on "ocaml"
 
