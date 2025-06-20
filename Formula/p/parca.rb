12fc04f14ef4a39998e06918b1903e3fc293ca3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 08:18:37 +0000

    parca 0.24.0

--- Formula/p/parca.rb
+++ Formula/p/parca.rb
@@ -1,8 +1,8 @@
 class Parca < Formula
   desc "Continuous profiling for analysis of CPU and memory usage"
   homepage "https://www.parca.dev/"
-  url "https://github.com/parca-dev/parca/archive/refs/tags/v0.23.1.tar.gz"
-  sha256 "fb5f0c1778e257cc1dd48e883ce0904535b0be25816b02dc61bc5b054eb822a5"
+  url "https://github.com/parca-dev/parca/archive/refs/tags/v0.24.0.tar.gz"
+  sha256 "a6e70f0687a583e6dfe5adfac9a05722bdc264c3c6017c54b713d8d57603f170"
   license "Apache-2.0"
   head "https://github.com/parca-dev/parca.git", branch: "main"
 
